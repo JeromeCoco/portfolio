81938a631e91996514fa2bb46499f4a569a0bca3
JeromeCoco <jeromcoco1@gmail.com>
2020-02-23 14:03:56 +0800

    Initial Setup

---
+++ js/script.js
@@ -0,0 +1,23 @@
+( function($) {
+
+    var app = {
+        
+    }
+
+    jQuery(document).ready( function() {
+		
+    });
+    
+    jQuery(window).on('load', function() {
+
+	});
+
+	jQuery(window).on('scroll', function() {
+		
+    });
+    
+    jQuery(window).on('resize', function() {
+		
+	});
+
+})( jQuery );
