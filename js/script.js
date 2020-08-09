c1093f3f1773834fac5cb89477ac0700e6719b2d
Jerome Coco <jerome.coco@august99.com>
2020-08-09 10:41:32 +0800

    Setup Coming Soon Page

--- js/script.js
+++ js/script.js
@@ -1,22 +1,74 @@
-( function($) {
+( function( $ ) {
 
     var app = {
-        
+		fixedHeader: {
+			init: function() {
+				this.scrollAppearance();
+			},
+			scrollAppearance: function() {
+				if (jQuery(window).scrollTop() >= 100) {
+					jQuery('header').addClass('scrolled');
+				} else {
+					jQuery('header').removeClass('scrolled');
+				}
+			}
+		},
+		aboutPopup: {
+			init: function() {
+				this.openPopup();
+				this.closePopup();
+			},
+			openPopup: function() {
+				$('.am-link a').click( function(e) {
+					e.preventDefault();
+
+					$('.am-popup-wrapper').addClass('open');
+					$('body').addClass('hide-scroll');
+				});
+			},
+			closePopup: function() {
+				$('.am-pop-close a').click( function(e) {
+					e.preventDefault();
+
+					$('.am-popup-wrapper').removeClass('open');
+					$('body').removeClass('hide-scroll');
+				});
+			},
+		},
+		jumpTrigger: {
+			init: function() {
+				this.jumpToAbout();
+			},
+			jumpToAbout: function() {
+				$('.banner-link a').click( function(e) {
+					e.preventDefault();
+
+					var about_section_position = $('.am-wrapper').offset().top;
+
+					$('html, body').animate({
+						scrollTop: about_section_position
+					});
+				});
+			}
+		}
     }
 
-    jQuery(document).ready( function() {
-		
+    $(document).ready( function() {
+		new WOW().init();
+		app.fixedHeader.init();
+		app.aboutPopup.init();
+		app.jumpTrigger.init();
     });
     
-    jQuery(window).on('load', function() {
+    $(window).on('load', function() {
 
 	});
 
-	jQuery(window).on('scroll', function() {
-		
+	$(window).on('scroll', function() {
+		app.fixedHeader.init();
     });
     
-    jQuery(window).on('resize', function() {
+    $(window).on('resize', function() {
 		
 	});
 
